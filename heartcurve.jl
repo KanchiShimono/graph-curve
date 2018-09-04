6e27ae341d8afd5be5868643048dd3f3ec20e97b
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2018-09-05 03:06:23 +0900

    Adjust looking of figure

--- heartcurve.jl
+++ heartcurve.jl
@@ -5,5 +5,8 @@ y(t::Real) = 13cos(t) - 5cos(2t) - 2cos(3t) - cos(4t)
 
 function main()
     t = range(0, stop=2pi, length=10000)
+    fig = figure("heart", figsize=(5,5))
+    xlim(-21, 21)
+    ylim(-18, 13)
     plot(x.(t), y.(t), color="red")
 end

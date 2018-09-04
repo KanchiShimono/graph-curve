9d325131b3a280ba97c044f4ee9dda304bb22098
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2018-09-05 03:05:01 +0900

    Update function style from array to a vaule
    Use vectorize function.

--- heartcurve.jl
+++ heartcurve.jl
@@ -1,9 +1,9 @@
 using PyPlot
 
-x(t) = 16sin.(t).^3
-y(t) = 13cos.(t) - 5cos.(2t) - 2cos.(3t) - cos.(4t)
+x(t::Real) = 16sin(t)^3
+y(t::Real) = 13cos(t) - 5cos(2t) - 2cos(3t) - cos(4t)
 
 function main()
     t = range(0, stop=2pi, length=10000)
-    plot(x(t), y(t))
+    plot(x.(t), y.(t), color="red")
 end
